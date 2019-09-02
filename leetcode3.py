3e8874e515339f2cbfe9acb2f4656d0927db8529
QSuHack <41905205+QSuHack@users.noreply.github.com>
2019-09-02 14:27:00 +0200

    Add files via upload
    Keyboard Row

--- leetcode3.py
+++ leetcode3.py
@@ -1,4 +1,4 @@
-# from typing import List
+from typing import List
 
 
 class Solution:
@@ -36,7 +36,88 @@ class Solution:
             index2 += 1
         return decimal_value
 
+    def defang_IP_addr(self, address: str) -> str:
+        """
+        Given a valid (IPv4) IP address, return a defanged version of that IP address.
+        """
+        return address.replace(".", "[.]")
 
-a = Solution()
+    def to_lower(self, string: str) -> str:
+        """
+        Change upper letter to lower
+        :param string: ONLY ASCII letter
+        :return:
+        """
+        new_string = ""
+        for char in string:
+            if char.isupper():
+                new_string = new_string + chr(ord(char) + 32)
+            else:
+                new_string = new_string + char
+        return new_string
+        # return string.lower() <- It also has 36ms runtime
 
-print(a.romanToInt(input()))
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        """
+        Return number of jewels in stones
+        :param J: representing the types of stones that are jewels
+        :param S: representing the stones you have
+        :return:number of jewels in stones (J in S, case-sensitive)
+        """
+        list_of_stones = list(S)
+        list_of_jewels = list(J)
+        num_of_jewels = 0
+        for jewel in list_of_jewels:
+            num_of_jewels += list_of_stones.count(jewel)
+        return num_of_jewels
+
+    def reverseVowels(self, s: str) -> str:
+        """
+        Return word with reserved vowels
+        :param s: str
+        :return: str with reserved vowels e.g. hello -> holle
+        """
+        new_string = ""
+        list_of_vowels = []
+        vowels = ["a", "e", "i", "o", "u","A","E","I", "O","U"]
+        for char in s:
+            if char in vowels:
+                list_of_vowels.append(char)
+        list_of_vowels = list_of_vowels[::-1]
+        index = 0
+        for char in s:
+            if char in vowels:
+                new_string = new_string + list_of_vowels[index]
+                index += 1
+            else:
+                new_string = new_string + char
+        return new_string
+
+    def find_words(self, words: List[str]) -> List[str]:
+        """
+        Return list of words (contains only alphabet letter) that can be typed using only one row of American keyboard
+        :param words: list of words
+        :return: List of words which  meet the condition.
+        """
+        first_row = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
+        second_row = ["a","s", "d", "f", "g", "h", "j", "k","l"]
+        third_row = ["z", "x", "c","v","b","n","m"]
+        keyboard = [first_row, second_row, third_row]
+        result = []
+        for word in words:
+            for row in keyboard:
+                is_one_row = True
+                for char in word:
+                    if char.lower() not in row:
+                        is_one_row = False
+                        break
+                if is_one_row:
+                    result.append(word)
+                    break
+        return result
+
+
+if __name__ == "__main__":
+    a = Solution()
+    print(a.find_words(["Allaska"]))
+    print(a.roman_to_int(input()))
