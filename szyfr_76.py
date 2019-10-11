13203b836f29047c0764de972ca38af35a522d85
QSuHack <41905205+QSuHack@users.noreply.github.com>
2019-10-11 19:06:04 +0200

    Decryption method
    Exercise 76.3

--- szyfr_76.py
+++ szyfr_76.py
@@ -9,6 +9,15 @@ def encrypt(information: List[str], key: List[int]) -> str:
     return "".join(information)
 
 
+def decrypt(information: List[str], key: List[int]) -> List[str]:
+    root_index = len(information)-1
+    for num in range(1, len(information)+1):
+        index = root_index % len(key)
+        information[-num], information[key[index]-1] = information[key[index]-1], information[-num]
+        root_index -= 1
+    return information
+
+
 def load_key(key: str) -> List[int]:
     return [int(x) for x in key.replace("\n", "").split(" ")]
 
@@ -32,4 +41,5 @@ if __name__ == "__main__":
             result_file.write(f"{encrypt(list(encrypted_one.readline()[0:-1]), load_key(encrypted_one.readline()))}")
         with open("szyfr3.txt") as encrypted_one:
             enc = encrypted_one.readline().replace("\n", "")
-            print(encrypt(list(enc.replace("\n", "")), [6, 2, 4, 1, 5, 3]))
+            result_file.write("\nZadanie 76.3\n")
+            result_file.write("".join(decrypt(list(enc.replace("\n", "")), [6, 2, 4, 1, 5, 3])))
